7b4ce5ea539399723e86d7a0aee9af4f460565e5
laurent collet <lcollet.dev@gmail.com>
2024-05-04 23:34:38 +0200

    Setup routes file and add few firebase files to gitignore

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomeView} from "./views/home-view/home-view.component";
+import {SessionView} from "./views/session-view/session-view.component";
+import {ErrorView} from "./views/error-view/error-view.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo: "home"},
+  {component: HomeView, path: "home"},
+  {component: SessionView, path: "sessions/:sessionId"},
+  {path: "error", component: ErrorView},
+  {path: "**", redirectTo: "error"},
+];
---
+++ src/app/views/session-view/session-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-session-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './session-view.component.html',
+  styleUrl: './session-view.component.scss'
+})
+export class SessionView{
+
+}
