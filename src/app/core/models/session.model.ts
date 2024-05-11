b98d94dfaaa168de64bd9226c866e88d5f3737ab
laurent collet <lcollet.dev@gmail.com>
2024-05-11 18:07:23 +0200

    Update session.model.ts

--- src/app/core/models/session.model.ts
+++ src/app/core/models/session.model.ts
@@ -1,9 +1,16 @@
 import {VotingSystem} from "./voting-system.model";
 import {Player} from "./player.model";
 
-export interface Session {
-  id : string;
-  name : string;
-  votingSystem : VotingSystem;
-  players : Player[];
+export class Session {
+  id: string;
+  name: string;
+  votingSystem: VotingSystem;
+  players: Player[];
+
+  constructor(id: string, name: string, votingSystem: VotingSystem, players: Player[]) {
+    this.id = id;
+    this.name = name;
+    this.votingSystem = votingSystem
+    this.players = players;
+  }
 }
